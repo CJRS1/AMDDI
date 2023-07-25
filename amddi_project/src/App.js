0a64df19ba0ef279dd4a2f11d1f62cebbe95bb4a
CJRS1 <reyes.christian@pucp.pe>
2023-07-25 14:58:11 -0700

    Se realizó el formulario para contáctanos

--- amddi_project/src/App.js
+++ amddi_project/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from './components/Header' 
 import Footer from './components/Footer'
 import Carousel from './components/Carousel'
+import Contacto from './components/Contacto'
 
 function App() {
   return (
     <div className='div-principal'>
       <Header />
       <Carousel />
+      <Contacto />
       <Footer />
     </div>
   );
