7e22f9458eacf904270f2328f35efbe200797a4a
CJRS1 <reyes.christian@pucp.pe>
2023-07-25 12:16:28 -0700

    Se realizó el footer

--- amddi_project/src/components/Carousel.js
+++ amddi_project/src/components/Carousel.js
@@ -9,7 +9,7 @@ export default function Carousel() {
                 <div className="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="0" className="active" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="1" aria-label="Slide 2" aria-current="true"></button>
-                    <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="2" aria-label="Slide 3" aria-current="true"></button>
                     <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="3" aria-label="Slide 4"></button>
                 </div>
                 <div className="carousel-inner">
