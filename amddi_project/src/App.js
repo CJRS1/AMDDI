cf1f5b2c3afcdf0c4b1e44f36a18f7d4ec2cd684
CJRS1 <reyes.christian@pucp.pe>
2023-07-27 12:21:57 -0700

    Se Realizó el enrutamiento Contáctanos

--- amddi_project/src/App.js
+++ amddi_project/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Carousel from './components/Carousel';
@@ -9,36 +11,36 @@ import Testimonios from './components/Testimonios';
 import Servicios from './components/Servicios';
 
 function App() {
+
   return (
-    <div className='div-principal'>
+    <Router>
+      
       <Header />
 
-      <Router>
-        <div>
-          <Navigation />
-          <Switch>
-            <Route path="/">
-              <Carousel />
-              <Servicios />
-            </Route>
-            <Route path="/servicios">
-              <Servicios />
-            </Route>
-            <Route path="/testimonios">
-              <Testimonios />
-            </Route>
-            <Route path="/contactanos">
-              <Contacto />
-            </Route>
-            <Route path="/nosotros">
-              <Nosotros />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      <Routes>
+
+        <Route path="/" element={
+          <>
+            <Carousel />
+            <Servicios />  
+          </>
+        } />
+
+        <Route path="/servicios" element={<Servicios />} />
+        
+        <Route path="/testimonios" element={<Testimonios />} />
+        
+        <Route path="/contactanos" element={<Contacto />} />
+        
+        <Route path="/nosotros" element={<Nosotros />} />
+
+      </Routes>
+
       <Footer />
-    </div>
+
+    </Router>
   );
+
 }
 
 export default App;
