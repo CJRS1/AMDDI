4ed49a21c56824b10388e7c64a91a43a0704a406
CJRS1 <reyes.christian@pucp.pe>
2023-07-31 14:07:05 -0700

    Se realizó el programa de ingresos extra

--- amddi_project/src/components/Programa.js
+++ amddi_project/src/components/Programa.js
@@ -13,15 +13,85 @@ export default function Testimonios() {
     return (
         <section className="ingresos_container">
             <div className="ingresos_description">
+                <div className="ingresos_card">
+                    <h2 className="titulo">Programa de Ingresos Extras</h2>
+                    <p>Gana ingresos extras desde casa
 
-            <h2>Programa de Ingresos Extras</h2>
-            <p>¿Tienes algún talento en áreas como diseño gráfico, redacción, programación, traducción u otras habilidades? ¡Gana ingresos extras desde la comodidad de tu hogar completando mini trabajos y proyectos freelance!
+                        Únete a nuestra comunidad freelance eligiendo entre:
 
-                Buscamos personas con diferentes habilidades para realizar trabajos cortos, no recurrentes y remunerados. Los proyectos pueden incluir diseño de gráficos, redacción creativa, programación web, entrada de datos, traducciones, transcripciones, etc.
+                        Contratos a tiempo completo para proyectos grandes.
+                        Mini trabajos y proyectos por horas o tareas puntuales.
+                        No necesitas experiencia previa. Postula con tus habilidades (diseño, redacción, programación, traducción, etc.)
 
-                No se requiere experiencia previa ni compromisos a largo plazo. Tú eliges los proyectos según tus habilidades e intereses. El pago es por proyecto completado satisfactoriamente.
+                        Tú eliges los proyectos y horarios. Perfecto para ingresos adicionales flexibles.
 
-                Únete a nuestra comunidad freelance y empieza a ganar ingresos extra desde casa en tu tiempo libre. Los horarios y días de trabajo son totalmente flexibles. ¡Es una oportunidad perfecta para estudiantes, amas de casa, jubilados o cualquier persona que busque generar un ingreso adicional!</p>
+                        Envíanos tu CV y rellena el formulario de postulación. Buscamos personas responsables para impulsar sus habilidades y generar ingresos extras desde casa.</p>
+                </div>
+
+                <form method="POST" action="https://formsubmit.co/reyes.christian@pucp.pe" encType="multipart/form-data" className="container-contacto-empresa programa-formulario">
+                    <h2 className="titulo"><strong>¡Postula ya! Rellena tus datos</strong></h2>
+
+                    <div className="container-contacto">
+                        <div className="mb-3 d-flex ">
+                            <input type="text" name="name" className="form-control my-input" style={{ width: '610px' }} id="Input1"
+                                placeholder="Nombre y apellido *" required />
+
+                        </div>
+                        <div className="mb-3 d-flex">
+
+                            <input type="email" name="email" className="form-control form-email my-input" style={{ width: '300px', marginRight: '10px' }}
+                                id="exampleFormControlInput1" placeholder="Correo electrónico *" required />
+                            <input type="number" name="phone" className="form-control my-input" style={{ width: '300px' }} id="Input1"
+                                placeholder="Celular *" min="10000000" max="999999999999" required />
+                        </div>
+                        <div className="mb-3 d-flex ">
+                            <input type="text" name="phone" className="form-control my-input" style={{ width: '300px', marginRight: '10px' }} id="Input1"
+                                placeholder="Puesto al que postula*" required />
+                            <select className="form-control my-input" name="departamento" style={{ width: '300px' }} id="contact-method" defaultValue="" required>
+                                <option value="" disabled >Seleccione un departamento</option>
+                                <option value="Ancash">Ancash</option>
+                                <option value="Apurímac">Apurímac</option>
+                                <option value="Arequipa">Arequipa</option>
+                                <option value="Ayacucho">Ayacucho</option>
+                                <option value="Cajamarca">Cajamarca</option>
+                                <option value="Callao">Callao</option>
+                                <option value="Cusco">Cusco</option>
+                                <option value="Huancavelica">Huancavelica</option>
+                                <option value="Huánuco">Huánuco</option>
+                                <option value="Ica">Ica</option>
+                                <option value="Junín">Junín</option>
+                                <option value="La Libertad">La Libertad</option>
+                                <option value="Lambayeque">Lambayeque</option>
+                                <option value="Lima">Lima</option>
+                                <option value="Loreto">Loreto</option>
+                                <option value="Madre de Dios">Madre de Dios</option>
+                                <option value="Moquegua">Moquegua</option>
+                                <option value="Pasco">Pasco</option>
+                                <option value="Piura">Piura</option>
+                                <option value="Puno">Puno</option>
+                                <option value="San Martín">San Martín</option>
+                                <option value="Tacna">Tacna</option>
+                                <option value="Tumbes">Tumbes</option>
+                                <option value="Ucayali">Ucayali</option>
+                            </select>
+                        </div>
+
+                        <div className="mb-3">
+                            <textarea className="form-control my-input" name="message" id="exampleFormControlTextarea1" style={{ width: '610px' }} rows="5"
+                                placeholder="Comentario o mensaje *" required></textarea>
+                        </div>
+                        <div class="mb-3 d-flex flex-column custom-file seleccionar-archivo">
+
+                            <input class="form-control btn color-button" name="file" type="file"
+                                id="formFileMultiple" accept=".pdf" required />
+                        </div>
+                        <button type="submit" className="btn button-contact">Enviar el formulario</button>
+
+                        <input type="hidden" name="_next" value="http://127.0.0.1:5500/contactanos.html" />
+                        <input type="hidden" name="_captcha" value="false" />
+
+                    </div>
+                </form>
             </div>
         </section >
     );
