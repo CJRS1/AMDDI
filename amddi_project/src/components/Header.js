c28f6efc9823ec1d42c6805c574e809aed17ab69
CJRS1 <reyes.christian@pucp.pe>
2023-08-02 12:41:43 -0700

    Se realizó el boton menu responsive

--- amddi_project/src/components/Header.js
+++ amddi_project/src/components/Header.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/Header.css'
 import { HashLink } from 'react-router-hash-link'
-
+import { useMediaQuery } from 'react-responsive';
 
 export default function Header() {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    function toggleMenu() {
+        setMenuOpen(!menuOpen);
+    }
+    function handleLinkClick() {
+        setMenuOpen(false);
+    }
+    const isMobile = useMediaQuery({ maxWidth: 768 });
+
+    const menuClass = isMobile ? 'dropdown-menu-end' : '';
 
     return (
         <header className="header_container">
@@ -12,58 +24,58 @@ export default function Header() {
                 <div className="container-fluid contenedor_logo_menu">
                     <Link to="/AMDDI" className="navbar-brand" href="#inicio"> <img className='img_header' src={require('../images/Logo_confondo.png')} alt='AMDDI' height={'80px'} />  </Link>
 
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation" onClick={toggleMenu} >
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul className="navbar-nav">
+                    <div className={`collapse navbar-collapse ${menuOpen ? 'menu-open' : 'menu-closed'}`} id="navbarNavDropdown" >
+                        <ul className="navbar-nav" >
                             {/* <li className="nav-item">
                                 <Link to="/AMDDI" className="nav-link active" aria-current="page" >Inicio </Link>
                             </li> */}
                             <li className="nav-item">
-                                <Link to="/AMDDI/nosotros" className="nav-link">Nosotros </Link>
+                                <Link to="/AMDDI/nosotros" className="nav-link" onClick={handleLinkClick}>Nosotros </Link>
                             </li>
                             <li className="nav-item dropdown">
                                 <Link to="/AMDDI/servicios" className="nav-link dropdown-toggle " role="button" data-bs-toggle="dropdown" aria-expanded="false" >
                                     Nuestros Servicios
                                 </Link>
-                                <ul className="dropdown-menu bg-gris" >
-                                    <li> <HashLink to="/AMDDI/servicios#carreras"  className="dropdown-item" >Carreras Asesoradas </HashLink> </li>
-                                    <li> <HashLink to="/AMDDI/servicios#redacciontesispregrado"  className="dropdown-item" >Redacción de Tesis de Pregrado </HashLink> </li>
-                                    <li> <HashLink to="/AMDDI/servicios#redacciontesispostgrado"  className="dropdown-item" >Redacción de Tesis de Postgrado </HashLink> </li>
-                                    <li> <HashLink to="/AMDDI/servicios#levantamientoobservacionespregrado"  className="dropdown-item" >Levantamiento de Observaciones Pregrado </HashLink> </li>
-                                    <li> <HashLink to="/AMDDI/servicios#levantamientoobservacionesmaestria"  className="dropdown-item" >Levantamiento de Observaciones Maestría </HashLink> </li>
-                                    <li> <HashLink to="/AMDDI/servicios#parafraseo"  className="dropdown-item" >Parafraseo </HashLink> </li>
-                                    <li> <HashLink to="/AMDDI/servicios#trabajosuficienciaprofesional"  className="dropdown-item" >Trabajo de Suficiencia Profesional </HashLink> </li>
-                                    <li> <HashLink to="/AMDDI/servicios#articulocientifico"  className="dropdown-item" >Artículo Científico </HashLink> </li>
-                                    <li> <HashLink to="/AMDDI/servicios#articulorevisionsistematica"  className="dropdown-item" >Artículo de Revisión Sistemática </HashLink> </li>
-                                    <li> <HashLink to="/AMDDI/servicios#articulorevisionliteratura"  className="dropdown-item" >Artículo de Revisión de Literatura </HashLink> </li>
-                                    <li> <HashLink to="/AMDDI/servicios#trabajodeciclo"  className="dropdown-item" >Trabajos de Ciclo </HashLink> </li>
-                                    <li> <HashLink to="/AMDDI/servicios#monografias"  className="dropdown-item" >Monografía </HashLink> </li>
-                                    <li> <HashLink to="/AMDDI/servicios#ensayos"  className="dropdown-item" >Ensayos </HashLink> </li>
-                                    <li> <HashLink to="/AMDDI/servicios#plannegocio"  className="dropdown-item" >Planes de Negocio </HashLink> </li>
-                                    <li> <HashLink to="/AMDDI/servicios#informepracticas" className="dropdown-item" >Informe de Prácticas </HashLink> </li>
-                                    <li> <HashLink to="/AMDDI/servicios#tesinas"  className="dropdown-item" >Tesinas </HashLink> </li>
-                                    <li> <HashLink to="/AMDDI/servicios#elaboracioncv"  className="dropdown-item" >Elaboración de CV </HashLink> </li>
-                                    <li> <HashLink to="/AMDDI/servicios#dispositivas"  className="dropdown-item" >Diapositivas </HashLink> </li>
-                                    <li> <HashLink to="/AMDDI/servicios#cursos"  className="dropdown-item" >Curso Online: próximamente... </HashLink> </li>
+                                <ul className={`dropdown-menu bg-gris ${menuClass}`} >
+                                    <li> <HashLink to="/AMDDI/servicios#carreras" className="dropdown-item" onClick={handleLinkClick}>Carreras Asesoradas </HashLink> </li>
+                                    <li> <HashLink to="/AMDDI/servicios#redacciontesispregrado" className="dropdown-item" onClick={handleLinkClick}>Redacción de Tesis de Pregrado </HashLink> </li>
+                                    <li> <HashLink to="/AMDDI/servicios#redacciontesispostgrado" className="dropdown-item" onClick={handleLinkClick}>Redacción de Tesis de Postgrado </HashLink> </li>
+                                    <li> <HashLink to="/AMDDI/servicios#levantamientoobservacionespregrado" className="dropdown-item" onClick={handleLinkClick}>Levantamiento de Observaciones Pregrado </HashLink> </li>
+                                    <li> <HashLink to="/AMDDI/servicios#levantamientoobservacionesmaestria" className="dropdown-item" onClick={handleLinkClick}>Levantamiento de Observaciones Maestría </HashLink> </li>
+                                    <li> <HashLink to="/AMDDI/servicios#parafraseo" className="dropdown-item" onClick={handleLinkClick}>Parafraseo </HashLink> </li>
+                                    <li> <HashLink to="/AMDDI/servicios#trabajosuficienciaprofesional" className="dropdown-item" onClick={handleLinkClick}>Trabajo de Suficiencia Profesional </HashLink> </li>
+                                    <li> <HashLink to="/AMDDI/servicios#articulocientifico" className="dropdown-item" onClick={handleLinkClick}>Artículo Científico </HashLink> </li>
+                                    <li> <HashLink to="/AMDDI/servicios#articulorevisionsistematica" className="dropdown-item" onClick={handleLinkClick}>Artículo de Revisión Sistemática </HashLink> </li>
+                                    <li> <HashLink to="/AMDDI/servicios#articulorevisionliteratura" className="dropdown-item" onClick={handleLinkClick}>Artículo de Revisión de Literatura </HashLink> </li>
+                                    <li> <HashLink to="/AMDDI/servicios#trabajodeciclo" className="dropdown-item" onClick={handleLinkClick}>Trabajos de Ciclo </HashLink> </li>
+                                    <li> <HashLink to="/AMDDI/servicios#monografias" className="dropdown-item" onClick={handleLinkClick}>Monografía </HashLink> </li>
+                                    <li> <HashLink to="/AMDDI/servicios#ensayos" className="dropdown-item" onClick={handleLinkClick}>Ensayos </HashLink> </li>
+                                    <li> <HashLink to="/AMDDI/servicios#plannegocio" className="dropdown-item" onClick={handleLinkClick}>Planes de Negocio </HashLink> </li>
+                                    <li> <HashLink to="/AMDDI/servicios#informepracticas" className="dropdown-item" onClick={handleLinkClick}>Informe de Prácticas </HashLink> </li>
+                                    <li> <HashLink to="/AMDDI/servicios#tesinas" className="dropdown-item" onClick={handleLinkClick}>Tesinas </HashLink> </li>
+                                    <li> <HashLink to="/AMDDI/servicios#elaboracioncv" className="dropdown-item" onClick={handleLinkClick}>Elaboración de CV </HashLink> </li>
+                                    <li> <HashLink to="/AMDDI/servicios#dispositivas" className="dropdown-item" onClick={handleLinkClick}>Diapositivas </HashLink> </li>
+                                    <li> <HashLink to="/AMDDI/servicios#cursos" className="dropdown-item" onClick={handleLinkClick}>Curso Online: próximamente... </HashLink> </li>
                                 </ul>
                             </li>
                             <li className="nav-item">
-                                <Link to="/AMDDI/ingresosextra" className="nav-link">Ingresos Extra </Link>
+                                <Link to="/AMDDI/ingresosextra" className="nav-link" onClick={handleLinkClick}>Ingresos Extra </Link>
                             </li>
                             <li className="nav-item">
-                                <Link to="/AMDDI/investigacionsostenible" className="nav-link">Investigación Sostenible</Link>
+                                <Link to="/AMDDI/investigacionsostenible" className="nav-link" onClick={handleLinkClick}>Investigación Sostenible</Link>
                             </li>
                             <li className="nav-item">
-                                <Link to="/AMDDI/emprendimientojoven" className="nav-link">Emprendimiento Joven </Link>
+                                <Link to="/AMDDI/emprendimientojoven" className="nav-link" onClick={handleLinkClick}>Emprendimiento Joven </Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/AMDDI/testimonios" className="nav-link">Testimonios </Link>
+                                <Link to="/AMDDI/testimonios" className="nav-link" onClick={handleLinkClick}>Testimonios </Link>
                             </li>
                             <li className="nav-item">
-                                <Link to="/AMDDI/contactanos" className="nav-link">Contáctanos </Link>
+                                <Link to="/AMDDI/contactanos" className="nav-link" onClick={handleLinkClick}>Contáctanos </Link>
                             </li>
                         </ul>
                     </div>
