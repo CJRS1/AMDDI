56087776235d2ac9c5b0542670b3d78e7e746907
CJRS1 <reyes.christian@pucp.pe>
2023-08-28 15:21:56 -0700

    Se añadieron más ubicaciones

--- amddi_project/src/components/Footer.js
+++ amddi_project/src/components/Footer.js
@@ -163,6 +163,30 @@ export default function Footer() {
                                 <h5>&nbsp;Jr. Bellido 534, 2do piso Oficina B</h5>
                             </a>
                         </div>
+                        <div className="card-icloc">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#00d799" className="bi bi-geo-alt"
+                                viewBox="0 0 16 16">
+                                <path
+                                    d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+                                <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                            </svg>
+                            <a href='https://goo.gl/maps/dAQbK5v6yx14id7L6' target='_blank' rel='noreferrer' className="card-localizacion">
+                                <h5>TRUJILLO</h5>
+                                <h5>&nbsp;Antonio Andueza 149, urbanización la Merced, Trujillo</h5>
+                            </a>
+                        </div>
+                        <div className="card-icloc">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#00d799" className="bi bi-geo-alt"
+                                viewBox="0 0 16 16">
+                                <path
+                                    d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+                                <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                            </svg>
+                            <a href='https://goo.gl/maps/LVMXphPBKPPq23feA' target='_blank' rel='noreferrer' className="card-localizacion">
+                                <h5>CHICLAYO</h5>
+                                <h5>&nbsp;Alfondo Ugarte 655, Entre Elias Aguirre y Maria Izaga</h5>
+                            </a>
+                        </div>
                     </div>
                     <div className="footer_contacto">
                         <div className="footer_contacto">
