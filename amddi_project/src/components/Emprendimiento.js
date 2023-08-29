84ca1f3dc01df9ed3f6b7deff830a9a8240120e1
CJRS1 <reyes.christian@pucp.pe>
2023-08-29 13:58:36 -0700

    Se realizaron los cambios pedidos en los formularios

--- amddi_project/src/components/Emprendimiento.js
+++ amddi_project/src/components/Emprendimiento.js
@@ -1,66 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect } from "react";
 import { useLocation } from 'react-router-dom';
 import '../styles/Emprendimiento.css';
 
 export default function Emprendimiento() {
     const location = useLocation();
     const [showAlert, setShowAlert] = useState(false);
-    const [formData, setFormData] = useState({
-        name: "",
-        email: "",
-        phone: "",
-        emprendimiento: "",
-        departamento: "",
-        message: ""
-    });
 
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [location]);
 
-    const handleFormSubmit = async (e) => {
-        e.preventDefault();
-
-        // Aquí realizarías la lógica real para enviar el formulario, por ejemplo, hacer la solicitud POST.
-        try {
-            const response = await fetch('https://formsubmit.co/info@amddi.com', {
-                method: 'POST',
-                // Aquí puedes configurar las cabeceras y datos del formulario
-            });
-
-            if (response.ok) {
-                setShowAlert(true);
-                setFormData({
-                    name: "",
-                    email: "",
-                    phone: "",
-                    emprendimiento: "",
-                    departamento: "",
-                    message: ""
-                });
-            } else {
-                console.error('Error al enviar el formulario');
-            }
-        } catch (error) {
-            console.error('Error al enviar el formulario', error);
-        }
+    const handleAlertClick = () => {
+        setShowAlert(true);
+        setTimeout(() => {
+            alert("Formulario enviado correctamente.");
+            setShowAlert(false);
+        }, 1000);
     };
 
-    useEffect(() => {
-        if (showAlert) {
-            // Muestra la alerta
-            alert("Formulario enviado exitosamente");
-            setShowAlert(false); // Cierra la alerta automáticamente después de mostrarla
-        }
-    }, [showAlert]);
-
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setFormData((prevFormData) => ({
-            ...prevFormData,
-            [name]: value
-        }));
-    };
     return (
         <section className="ingresos_container">
 
@@ -72,27 +29,27 @@ export default function Emprendimiento() {
                 </div>
                 <div className="form_ingreso">
 
-                    <form  onSubmit={handleFormSubmit}  method="POST" action="https://formsubmit.co/info@amddi.com" encType="multipart/form-data" className="container-contacto-empresa programa-formulario">
+                    <form   method="POST" action="https://formsubmit.co/info@amddi.com" encType="multipart/form-data" className="container-contacto-empresa programa-formulario" >
                         <h3 className="titulo"><strong>Únete y convierte tu visión en realidad</strong></h3>
                         <h4> ¡Postula ya! Rellena tus datos en el formulario</h4>
 
                         <div className="container-contacto">
                             <div className="mb-3 d-flex ">
                                 <input type="text" name="name" className="form-control my-input" style={{ width: '510px' }} id="Input1"
-                                    placeholder="Nombre y apellido *" value={formData.name} onChange={handleInputChange} required />
+                                    placeholder="Nombre y apellido *"  required />
 
                             </div>
                             <div className="mb-3 d-flex input_contact_container">
 
-                                <input type="email" name="email" value={formData.email} onChange={handleInputChange} className="form-control form-email my-input" style={{ width: '250px', marginRight: '10px' }}
+                                <input type="email" name="email"  className="form-control form-email my-input" style={{ width: '250px', marginRight: '10px' }}
                                     id="exampleFormControlInput1" placeholder="Correo electrónico *" required />
-                                <input type="number" name="phone" value={formData.phone} onChange={handleInputChange} className="form-control my-input input_card" style={{ width: '250px' }} id="Input1"
+                                <input type="number" name="phone"  className="form-control my-input input_card" style={{ width: '250px' }} id="Input1"
                                     placeholder="Celular *" min="10000000" max="999999999999" required />
                             </div>
                             <div className="mb-3 d-flex input_contact_container">
-                                <input type="text" name="emprendimiento" value={formData.emprendimiento} onChange={handleInputChange} className="form-control my-input" style={{ width: '250px', marginRight: '10px' }} id="Input1"
+                                <input type="text" name="emprendimiento"  className="form-control my-input" style={{ width: '250px', marginRight: '10px' }} id="Input1"
                                     placeholder="Nombre del Emprendimiento*" required />
-                                <select className="form-control my-input input_card" name="departamento" value={formData.departamento} onChange={handleInputChange} style={{ width: '250px' }} id="contact-method" defaultValue="" required>
+                                <select className="form-control my-input input_card" name="departamento"  style={{ width: '250px' }} id="contact-method" defaultValue="" required>
                                     <option value="" disabled >Seleccione un departamento</option>
                                     <option value="Ancash">Ancash</option>
                                     <option value="Apurímac">Apurímac</option>
@@ -122,11 +79,18 @@ export default function Emprendimiento() {
                             </div>
 
                             <div className="mb-3">
-                                <textarea className="form-control my-input" value={formData.message} onChange={handleInputChange} name="message" id="exampleFormControlTextarea1" style={{ width: '510px' }} rows="5"
+                                <textarea className="form-control my-input"  name="message" id="exampleFormControlTextarea1" style={{ width: '510px' }} rows="5"
                                     placeholder="Mi Proyecto de Emprendimiento Joven trata sobre ... *" required></textarea>
                             </div>
-
-                            <button type="submit" className="btn button-contact">Enviar el formulario</button>
+                            {showAlert && (
+                                <div className="alert-overlay">
+                                    <div className="alert-box">
+                                        <p>¡Espere un momento, por favor!</p>
+                                        <button className="alert-button" onClick={() => setShowAlert(false)}>OK</button>
+                                    </div>
+                                </div>
+                            )}
+                            <button type="submit" className="btn button-contact" onClick={handleAlertClick}>Enviar el formulario</button>
 
                             <input type="hidden" name="_next" value="https://www.amddi.com/emprendimientojoven" />
                             <input type="hidden" name="_captcha" value="false" />
--- amddi_project/src/components/Investigacion.js
+++ amddi_project/src/components/Investigacion.js
@@ -5,61 +5,17 @@ import '../styles/Investigacion.css';
 export default function Investigacion() {
     const location = useLocation();
     const [showAlert, setShowAlert] = useState(false);
-    const [formData, setFormData] = useState({
-        name: "",
-        email: "",
-        phone: "",
-        emprendimiento: "",
-        departamento: "",
-        message: ""
-    });
 
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [location]);
 
-    const handleFormSubmit = async (e) => {
-        e.preventDefault();
-
-        // Aquí realizarías la lógica real para enviar el formulario, por ejemplo, hacer la solicitud POST.
-        try {
-            const response = await fetch('https://formsubmit.co/info@amddi.com', {
-                method: 'POST',
-                // Aquí puedes configurar las cabeceras y datos del formulario
-            });
-
-            if (response.ok) {
-                setShowAlert(true);
-                setFormData({
-                    name: "",
-                    email: "",
-                    phone: "",
-                    investigacion: "",
-                    departamento: "",
-                    message: ""
-                });
-            } else {
-                console.error('Error al enviar el formulario');
-            }
-        } catch (error) {
-            console.error('Error al enviar el formulario', error);
-        }
-    };
-
-    useEffect(() => {
-        if (showAlert) {
-            // Muestra la alerta
-            alert("Formulario enviado exitosamente");
-            setShowAlert(false); // Cierra la alerta automáticamente después de mostrarla
-        }
-    }, [showAlert]);
-
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setFormData((prevFormData) => ({
-            ...prevFormData,
-            [name]: value
-        }));
+    const handleAlertClick = () => {
+        setShowAlert(true);
+        setTimeout(() => {
+            alert("Formulario enviado correctamente.");
+            setShowAlert(false);
+        }, 1000);
     };
 
     return (
@@ -67,32 +23,32 @@ export default function Investigacion() {
 
             <div className="ingresos_description investigacion">
                 <div className="investigacion_card">
-                <img className="img-programa" src={require('../images/Programa/invsostenible.png')} alt='proyecto' />
+                    <img className="img-programa" src={require('../images/Programa/invsostenible.png')} alt='proyecto' />
                     {/* <h2 className="titulo"><strong>Programa de Investigación Sostenible</strong></h2>
                     <p> ¡Bienvenido al Programa de Investigación Sostenible AMDDI! Apoyamos a jóvenes investigadores comprometidos con soluciones innovadoras para los desafíos ambientales, sociales y económicos de nuestro país. Si compartes esta misión, te conectaremos con mentores expertos que te guiarán en tus investigaciones, brindándote valiosa orientación. También reconocemos la importancia de contar con recursos, por eso financiamos proyectos alineados con nuestros objetivos de desarrollo sostenible. Únete a nuestro programa y sé parte del cambio que el país necesita. Juntos podemos construir un futuro más sostenible. </p> */}
                 </div>
                 <div className="form_ingreso">
-                    <form onSubmit={handleFormSubmit}  method="POST" action="https://formsubmit.co/info@amddi.com" encType="multipart/form-data" className="container-contacto-empresa programa-formulario">
+                    <form method="POST" action="https://formsubmit.co/info@amddi.com" encType="multipart/form-data" className="container-contacto-empresa programa-formulario">
                         <h3 className="titulo"><strong>Construyamos un futuro más sostenible</strong></h3>
                         <h4> ¡Postula ya! Rellena tus datos en el formulario</h4>
 
                         <div className="container-contacto">
                             <div className="mb-3 d-flex ">
                                 <input type="text" name="name" className="form-control my-input" style={{ width: '510px' }} id="Input1"
-                                    placeholder="Nombre y apellido *" value={formData.name} onChange={handleInputChange} required />
+                                    placeholder="Nombre y apellido *" required />
 
                             </div>
                             <div className="mb-3 d-flex input_contact_container">
 
                                 <input type="email" name="email" className="form-control form-email my-input" style={{ width: '250px', marginRight: '10px' }}
-                                    id="exampleFormControlInput1" placeholder="Correo electrónico *" value={formData.email} onChange={handleInputChange} required />
+                                    id="exampleFormControlInput1" placeholder="Correo electrónico *" required />
                                 <input type="number" name="phone" className="form-control my-input input_card" style={{ width: '250px' }} id="Input1"
-                                    placeholder="Celular *" min="10000000" max="999999999999" value={formData.phone} onChange={handleInputChange} required />
+                                    placeholder="Celular *" min="10000000" max="999999999999" required />
                             </div>
                             <div className="mb-3 d-flex input_contact_container">
                                 <input type="text" name="investigacion" className="form-control my-input" style={{ width: '250px', marginRight: '10px' }} id="Input1"
-                                    placeholder="Nombre de la Investigación*" value={formData.investigacion} onChange={handleInputChange} required />
-                                <select className="form-control my-input input_card" name="departamento" style={{ width: '250px' }} id="contact-method" defaultValue="" value={formData.departamento} onChange={handleInputChange} required>
+                                    placeholder="Nombre de la Investigación*" required />
+                                <select className="form-control my-input input_card" name="departamento" style={{ width: '250px' }} id="contact-method" defaultValue="" required>
                                     <option value="" disabled >Seleccione un departamento</option>
                                     <option value="Ancash">Ancash</option>
                                     <option value="Apurímac">Apurímac</option>
@@ -123,15 +79,24 @@ export default function Investigacion() {
 
                             <div className="mb-3">
                                 <textarea className="form-control my-input" name="message" id="exampleFormControlTextarea1" style={{ width: '510px' }} rows="5"
-                                    placeholder="Mi Proyecto de Investigación Sostenible trata sobre ... *" value={formData.message} onChange={handleInputChange}required></textarea>
+                                    placeholder="Mi Proyecto de Investigación Sostenible trata sobre ... *" required></textarea>
                             </div>
+                            {showAlert && (
+                                <div className="alert-overlay">
+                                    <div className="alert-box">
+                                        <p>¡Espere un momento, por favor!</p>
+                                        <button className="alert-button" onClick={() => setShowAlert(false)}>OK</button>
+                                    </div>
+                                </div>
+                            )}
 
-                            <button type="submit" className="btn button-contact">Enviar el formulario</button>
+                            <button type="submit" className="btn button-contact" onClick={handleAlertClick}>Enviar el formulario</button>
 
                             <input type="hidden" name="_next" value="https://www.amddi.com/investigacionsostenible" />
                             <input type="hidden" name="_captcha" value="false" />
 
                         </div>
+
                     </form>
                 </div>
             </div>
